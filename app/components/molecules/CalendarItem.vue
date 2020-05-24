a157a288dbcdb4fa314e8054dab2ea2d51f56802
Alexandre <alexandre.combemorel@gmail.com>
2020-05-24 22:26:46 +1000

    make ui home page

---
+++ app/components/molecules/CalendarItem.vue
@@ -0,0 +1,38 @@
+<template>
+  <StackLayout class="calendar-item" :class="state">
+    <Label text="Mar" class="calendar-item__day"/>
+    <Label text="20" class="calendar-item__date"/>
+    <label text="Dec" class="calendar-item__month"/>
+  </StackLayout>
+</template>
+
+<script>
+export default {
+  props: ["state"]
+}
+</script>
+
+<style scoped lang="scss">
+@import '~/assets/css/variables.scss';
+.calendar-item {
+  width: 40;
+  text-align: center;
+  font-size: 14;
+  margin-right: $size-l;
+  &__date {
+    padding-top: 6;
+    height: 40;
+    margin: $size-s 0;
+    border-color: white;
+    border-width: 2;
+    border-radius: 50;
+    font-size: 16;
+  }
+  &.disabled {
+    color: #B3B3B3;
+    .calendar-item__date {
+      border-color: #B3B3B3;
+    }
+  }
+}
+</style>
--- app/components/views/Home.vue
+++ app/components/views/Home.vue
@@ -1,9 +1,42 @@
 <template>
   <GridLayout rows="auto, *" class="home">
-    <Title row="0" :content="`Welcome`" class="home__title"/>
+    <Title row="0" :content="`WELCOME`" class="home__title"/>
     <ScrollView row="1" @scroll="getScroll" orientation="vertical" class="home__section">
       <StackLayout class="home__wrapper">
-        <SectionTitle :content="`Map`"/>
+        <SectionTitle :content="`OUVERTURE`" class="home__section-title"/>
+        <StackLayout class="home__opening">
+          <StackLayout class="home__opening__calendar">
+            <calendar-item/>
+            <calendar-item/>
+            <calendar-item state="disabled"/>
+            <calendar-item state="disabled"/>
+            <calendar-item/>
+            <calendar-item/>
+          </StackLayout>
+          <GridLayout columns="auto, *" rows="auto, auto" class="home__opening__content">
+            <StackLayout rowSpan="2" col="0" class="home__opening__content__clock"></StackLayout>
+            <Title row="0" col="1" :content="`Mercredi`" class="home__opening__content__title"/>
+            <StackLayout row="1" col="1" class="home__opening__content__info">
+              <StackLayout class="home__opening__content__info__item" orientation="vertical">
+                <Label text="7h - 13h" class="home__opening__content__info__item__text"/>
+                <Label text="Bourges" class="home__opening__content__info__item__text"/>
+                <Label text="Marché de la chancellerie" class="home__opening__content__info__item__text"/>
+              </StackLayout>
+              <StackLayout class="home__opening__content__info__item" orientation="vertical">
+                <Label text="7h - 13h" class="home__opening__content__info__item__text"/>
+                <Label text="Bourges" class="home__opening__content__info__item__text"/>
+                <Label text="Marché de la chancellerie" class="home__opening__content__info__item__text"/>
+              </StackLayout>
+            </StackLayout>
+          </GridLayout>
+          <FlexboxLayout class="home__opening__content-description">
+            <StackLayout class="home__opening__content-description__item">
+              <Label text="Evenement :" class="home__opening__content-description__item__title"/>
+              <Label text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla rhoncus, risus in convallis vehicula, nisl leo mollis orci, id vestibulum sem enim maximus enim. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Morbi nibh sapien, ultricies eget turpis ut, interdum molestie purus. " class="home__opening__content-description__item__text"/>
+            </StackLayout>
+          </FlexboxLayout>
+        </StackLayout>
+        <SectionTitle :content="`MAP`" class="home__section-title"/>
         <MapView class="home__map" iosOverflowSafeArea="true" @mapReady="onMapReady"></MapView>
       </StackLayout>
     </ScrollView>
@@ -15,18 +48,19 @@ import { isAndroid, isIOS } from "tns-core-modules/platform";
 var mapsModule = require("nativescript-google-maps-sdk");
 
 import SectionTitle from "../molecules/SectionTitle";
+import CalendarItem from "../molecules/CalendarItem";
 import Title from "../atoms/Title";
 
 export default {
   components: {
-    SectionTitle, Title
+    SectionTitle, Title, CalendarItem
   },
   methods: {
     onMapReady(args) {
       this.mapView = args.object;
       this.mapView.zoomGesturesEnabled = true;
       const gMap = this.mapView.gMap;
-      this.mapView.latitude = 47.082692
+      this.mapView.latitude = 47.082692;
       this.mapView.longitude = 2.392121;
       this.mapView.zoom = 10;
       if (isAndroid) {
@@ -56,9 +90,65 @@ export default {
 </script>
 
 <style scoped lang="scss">
+@import '~/assets/css/variables.scss';
 .home {
   width: 100%;
   height: 100%;
+  &__title {
+    text-align:center;
+    margin: $size-xxl 0;
+  }
+  &__section-title {
+    margin-bottom: $size-l;
+  }
+  &__opening {
+    width:100%;
+    &__calendar {
+      margin: 0 $size-m;
+      height: 100;
+      orientation: horizontal;
+    }
+    &__content {
+      margin: 0 $size-l;
+      padding: 0 $size-m;
+      &__clock {
+        width: 115;
+        background: grey;
+        margin-right: $size-xxl;
+      }
+      &__title {
+        margin-bottom: $size-l;
+      }
+      &__info {
+        &__item {
+          text-align: right;
+          margin-top: $size-xl;
+          &__text {
+            margin-top: $size-m;
+            font-size: 20;
+            text-wrap: true;
+          }
+        }
+      }
+    }
+    &__content-description {
+      margin: 0 $size-l;
+      margin-top: $size-l;
+      margin-bottom: $size-xxl;
+      &__item {
+        border-color: white;
+        border-width: 2;
+        padding: $size-m;
+        &__title {
+          font-size: 18;
+        }
+        &__text {
+          margin-top: $size-m;
+          text-wrap: true;
+        }
+      }
+    }
+  }
   &__map {
     width: 300;
     height: 300;
